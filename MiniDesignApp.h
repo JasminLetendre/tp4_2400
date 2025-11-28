07452f15cb5709934443d25ec6d63e5d36702fd1
Jasmin Letendre <jasmin.letendre@etud.polymtl.ca>
2025-11-28 10:07:56 -0500

    Added last class

---
+++ MiniDesignApp.h
@@ -0,0 +1,20 @@
+#ifndef MINIDESIGNAPP_H
+#define MINIDESIGNAPP_H
+#include "ModeleOrthese.h"
+#include "AffichageStrategy.h"
+#include "SurfaceCreationStrategy.h"
+#include "GestionnaireCommandes.h"
+#include "CommandFactory.h"
+class MiniDesignApp {
+private:
+    ModeleOrthese modele;
+    AffichageStrategy* affichageStrategy;
+    SurfaceCreationStrategy* surfaceStrategy;
+    GestionnaireCommandes gestionnaireCommandes;
+    CommandFactory commandFactory;
+public:
+    MiniDesignApp(AffichageStrategy* affichage, SurfaceCreationStrategy* surface);
+    ~MiniDesignApp();
+    void executerBoucle();
+};
+#endif
